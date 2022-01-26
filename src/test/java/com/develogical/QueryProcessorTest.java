211009b3e44d8acc12ceeddfd382821a92aa9cd1
Marine De Backer <marine.de-backer@student-cs.fr>
2022-01-26 15:47:53 +0000

    Marine added

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -30,4 +30,11 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+    
+    @Test
+    public void knowsAboutWicked() throws Exception {
+        assertThat(queryProcessor.process("Wicked"),
+                containsString("musical"));
+    }
+    
 }
